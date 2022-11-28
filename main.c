1300faad64178baf6264d207f08b9084df974dd2
alon <alonsuissa12@gmail.com>
2022-11-28 15:53:08 +0200

    theerd try tests

--- main.c
+++ main.c
@@ -20,36 +20,46 @@ int main(){
                 }
             }
             scanf("%d%d", &i, &j);
-            int exist = pathExistence(copy, i, j);
-            if (exist == 1) {
-                printf("True\n");
-            } else {
+            if(i == j){
                 printf("False\n");
+            }
+            else {
+                int exist = pathExistence(copy, i, j);
+                if (exist == 1) {
+                    printf("True\n");
+                } else {
+                    printf("False\n");
+                }
             }
         }
         if (flag == 'C') {
-           scanf("%d%d", &i, &j);
-              for (int k = 0; k < 10; k++) {
-                for (int t = 0; t < 10; t++) {
-                    copy[k][t] = m[k][t];
-                }
+            scanf("%d%d", &i, &j);
+            if (i == j) {
+                printf("-1\n");
             }
-           int exist = pathExistence(copy, i, j);
-           if(exist == 0){
-               printf("-1\n");
-           }
-           else {
-               int visited[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-               int pathLength[] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
-               int previos[] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
-               pathLength[i] = 0;
-               shortestPath(i, j, m, 1, visited, pathLength, previos);
+            else
+            {
+                for (int k = 0; k < 10; k++) {
+                    for (int t = 0; t < 10; t++) {
+                        copy[k][t] = m[k][t];
+                    }
+                }
+                int exist = pathExistence(copy, i, j);
+                if (exist == 0) {
+                    printf("-1\n");
+                } else {
+                    int visited[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+                    int pathLength[] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+                    int previos[] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+                    pathLength[i] = 0;
+                    shortestPath(i, j, m, 1, visited, pathLength, previos);
 //               if (previos[j] == -1) {
 //                   printf("-1\n");
 //               }
-               char pathLengthChar = pathLength[j] + '0';
-               printf("%c\n", pathLengthChar);
-           }
+                    char pathLengthChar = pathLength[j] + '0';
+                    printf("%c\n", pathLengthChar);
+                }
+            }
         }
 
 //        if (flag == 'P') {
